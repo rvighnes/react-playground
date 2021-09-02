a3585abf4d6eedfeac41b047d2aaab91e9f5f559
Vighnesh Raut <rvighnes@amazon.com>
2021-09-02 11:35:57 +0000

    sakjndsk

---
+++ src/components/Timeline/Timeline.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface TimelineProps {
+}
+
+const Timeline: React.FC<TimelineProps> = (props) => {
+  return (
+    <div>
+      This is the timeline component
+    </div>
+  );
+};
+
+export default Timeline;
